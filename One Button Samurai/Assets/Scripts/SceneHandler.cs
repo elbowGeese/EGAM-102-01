7da6511a13ad7326eea26f23a07cf80f036ae75a
elbowGeese <eapricemail@gmail.com>
2025-02-13 13:49:45 -0800

    Scene Change Event Added
    on scene change the function invokes its event to better communicate between scripts in the future. some visuals changed to be less niche

--- One Button Samurai/Assets/Scripts/SceneHandler.cs
+++ One Button Samurai/Assets/Scripts/SceneHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneHandler : MonoBehaviour
 {
+    public static event Action onSceneChange;
+
     // loads a scene based on name given
     public void GoToScene(string sceneName)
     {
+        onSceneChange?.Invoke();
+
         SceneManager.LoadScene(sceneName);
     }
 }
